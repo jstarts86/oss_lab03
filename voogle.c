deb0843b1e5907a809eff8f4ee267532525a1ded
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-04-04 19:42:25 +0900

    got rid of read_content

--- voogle.c
+++ voogle.c
@@ -50,58 +50,12 @@ char * read_a_line ()
 	} while (buf_n > 0) ;
 	return s ;
 }
-char * read_content ()
-{
-	static char buf[BUFSIZ] ;
-	static int buf_n = 0 ;
-	static int curr = 0 ;
-
-	if (feof(fp_niv) && curr == buf_n - 1)
-		return 0x0 ;
-
-	char * s = 0x0 ;
-	size_t s_len = 0 ;
-	do {
-		int end = curr ;
-		while (!(end >= buf_n || !iscntrl(buf[end]))) {
-			end++ ;
-		}
-		if (curr < end && s != 0x0) {
-			curr = end ;
-			break ;
-		}
-		curr = end ;
-		while (!(end >= buf_n || iscntrl(buf[end]))) {
-			end++ ;
-		}
-		if (curr < end) {
-			if (s == 0x0) {
-				s = strndup(buf + curr, end - curr) ;
-				s_len = end - curr ;
-			}
-			else {
-				s = realloc(s, s_len + end - curr + 1) ;
-				s = strncat(s, buf + curr, end - curr) ;
-				s_len = s_len + end - curr ;
-			}
-		}
-		if (end < buf_n) {
-			curr = end + 1 ;
-			break ;
-		}
-
-		buf_n = fread(buf, 1, sizeof(buf), fp_niv) ;
-		curr = 0 ;
-	} while (buf_n > 0) ;
-	char * content = remove_book_verse(s);
-	return content;
-}
 
 char * checker_for_token_star (char * token) {
 	fp_niv = fopen("NIV.txt", "r") ;
 	char * output = 0x0 ;
 	char * s = 0x0 ;
-	while ((s = read_content())) {
+	while ((s = read_a_line())) {
 		if (strstr(s, token) != NULL) {
 			output = s;
 		}
@@ -110,7 +64,7 @@ char * checker_for_token_star (char * token) {
 	return output;
 }
 
-char* remove_book_verse(char* str) {
+char * remove_book_verse(char * str) {
     int space_count = 0;
     char *ptr = str;
     while (*ptr != '\0') {
@@ -128,6 +82,9 @@ char* remove_book_verse(char* str) {
     strcpy(new_str, str);
     return new_str;
 }
+
+
+
 int main (int argc, char ** argv)
 {
 
