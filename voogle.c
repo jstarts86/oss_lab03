4d399db20c09622b6288745a5f78e76ee9c7c363
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-04-04 19:24:14 +0900

    Succesful read_content

--- voogle.c
+++ voogle.c
@@ -50,38 +50,83 @@ char * read_a_line ()
 	} while (buf_n > 0) ;
 	return s ;
 }
+char * read_content ()
+{
+	static char buf[BUFSIZ] ;
+	static int buf_n = 0 ;
+	static int curr = 0 ;
 
-char * checker_for_token_star () {
+	if (feof(fp_niv) && curr == buf_n - 1)
+		return 0x0 ;
+
+	char * s = 0x0 ;
+	size_t s_len = 0 ;
+	do {
+		int end = curr ;
+		while (!(end >= buf_n || !iscntrl(buf[end]))) {
+			end++ ;
+		}
+		if (curr < end && s != 0x0) {
+			curr = end ;
+			break ;
+		}
+		curr = end ;
+		while (!(end >= buf_n || iscntrl(buf[end]))) {
+			end++ ;
+		}
+		if (curr < end) {
+			if (s == 0x0) {
+				s = strndup(buf + curr, end - curr) ;
+				s_len = end - curr ;
+			}
+			else {
+				s = realloc(s, s_len + end - curr + 1) ;
+				s = strncat(s, buf + curr, end - curr) ;
+				s_len = s_len + end - curr ;
+			}
+		}
+		if (end < buf_n) {
+			curr = end + 1 ;
+			break ;
+		}
+
+		buf_n = fread(buf, 1, sizeof(buf), fp_niv) ;
+		curr = 0 ;
+	} while (buf_n > 0) ;
+	char * content = remove_book_verse(s);
+	return content;
+}
+
+char * checker_for_token_star (char * token) {
 	fp_niv = fopen("NIV.txt", "r") ;
 	char * output = 0x0 ;
 	char * s = 0x0 ;
-	char * token = "Jude";
-	while ((s = read_a_line())) {
-		char * b_verse = strstr(s, " ");
-		char * verse = strstr(b_verse, " ");
-		if (strstr(b_verse, token) != NULL) {
+	while ((s = read_content())) {
+		if (strstr(s, token) != NULL) {
 			output = s;
-
 		}
 	fclose(fp_niv) ;
 	}
 	return output;
 }
 
-void remove_book_verse(char * line, char * final_str) {
-    char *first_space = strchr(line, ' ');
-    char buffer_str[1000];
-
-
-    if (first_space != NULL) {
-        strcpy(buffer_str, first_space + 1);
-    }  
-    char *final_space = strchr(buffer_str, ' ');
-
-    if (final_space != NULL) {
-        strcpy(final_str, final_space + 1);
-    } 
-   
+char* remove_book_verse(char* str) {
+    int space_count = 0;
+    char *ptr = str;
+    while (*ptr != '\0') {
+        if (*ptr == ' ') {
+            space_count++;
+            if (space_count == 2) {
+                char *new_str = (char*)malloc(strlen(ptr));
+                strcpy(new_str, ptr+1);
+                return new_str;
+            }
+        }
+        ptr++;
+    }
+    char *new_str = (char*)malloc(strlen(str));
+    strcpy(new_str, str);
+    return new_str;
 }
 int main (int argc, char ** argv)
 {
@@ -99,14 +144,13 @@ int main (int argc, char ** argv)
 	// }
 	char * s = 0x0 ;
 	char * token = "Jude";
-	char * mod_s = 0x0;
 	while ((s = read_a_line())) {
 		char * b_verse = strchr(s, ' ');
 		char * verse = strchr(b_verse, ' ');
 		if (strstr(b_verse, token) != NULL) {
-			remove_book_verse(s,mod_s);
+			char * mod_s = remove_book_verse(s);
 			printf("%s\n", mod_s) ;
-			free(s) ;
+			free(mod_s) ;
 		}
 	
 	}
