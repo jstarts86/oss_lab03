158db779aeb7522fe42d2b189346fa816a886057
jstarts86 <67910821+jstarts86@users.noreply.github.com>
2023-04-05 13:30:46 +0900

    working checker_token_star but remove first not

--- voogle.c
+++ voogle.c
@@ -7,12 +7,15 @@
 #define MAX_NUM_INPUT 8
 
 FILE * fp_niv ;
+char * read_a_line ();
 char * stringLower(char * str); 
 int checker_for_token (char * term, char * string);
 int checker_for_token_star (char * term, char * string);
 int checker_for_token_dash (char * term, char * string);
 char * remove_book_verse(char * str);
-int tokenizer(char * input, char ** terms, int max_terms)
+int tokenizer(char *input, char **terms);
+char* remove_last_char(const char* str);
+char* remove_first_char(const char* str);
 char * read_a_line ()
 {
 	static char buf[BUFSIZ] ;
@@ -60,31 +63,40 @@ char * read_a_line ()
 }
 
 
-int tokenizer(char * input, char ** terms, int max_terms){
-	const char quote[2] = "\"";
-	const char space[2] = " ";
-	int i = 0;
-	terms[i] = strtok(input, space);
+int tokenizer(char *input, char **terms) {
+    int i = 0;
+    int quotes = 0;
+    char *token_start = input;
 
+    strtok(input, "\n");
 
-	int i = 0;
-	while (terms[i] != NULL && i < max_terms) {
-		if(strcmp(terms[i], quote) == 0) {
-			terms[i] = strtok(NULL, space);
-			char * quotedWord[MAX_INPUT_LENGTH];
-			quotedWord[0] = '\0';
-			while(strcmp(terms[i], quote) != 0) {
-				strcat(quotedWord, terms[i]);
-				terms[i] = strtok(NULL, space);
-			}
-			strcpy(inputs[i], )
-		}
-		i++;
-		tokens[i] = strtok(NULL, " ");
-	}
-	return inputs;
-	
+    while (*input != '\0') {
+        if (*input == '\"') {
+            quotes = !quotes;
+        }
+
+        if (*input == ' ' && !quotes) {
+            *input = '\0';
+            terms[i++] = token_start;
+            token_start = input + 1;
+        }
+
+        input++;
+    }
+
+    // handle last token
+    if (i < MAX_NUM_INPUT && token_start != input) {
+        terms[i++] = token_start;
+    }
+
+    if (i > MAX_NUM_INPUT) {
+        printf("Error: Input contains more than %d tokens.\n", MAX_NUM_INPUT);
+        return -1;
+    }
+
+    return i;
 }
+
 
 //iterates through a string making all letters lower case using tolower
 char * stringLower(char * str) {
@@ -125,32 +137,39 @@ int checker_for_token (char * term, char * string) {
 	  verse whose prefix matches with token in case-insensitive way
 */
 int checker_for_token_star (char * term, char * string) {
+	
 	char * new_string = remove_book_verse(string);
 	const char space[2] = " ";
 	char * token = strtok(new_string, space);
 	char * replacement = strdup(term);
 	char * lowerTerm = stringLower(replacement);
 	int term_length = strlen(term);
-	
-	while (token != NULL) {
-		int result1 = strncmp(token, term, term_length);
-		char * lowerToken = stringLower(token);
-
-		int strcmp_lower_result = strcmp(lowerToken, lowerTerm);
-		int resultLower = strncmp(lowerToken, lowerTerm, term_length);
-		if((strcmp(token, term)) != 0) {
-			if(result1 == 0) {
-				return 0;
+	if(term[term_length-1] == '*') {
+		term = remove_last_char(term);
+		term_length = strlen(term);
+		while (token != NULL) {
+			int result1 = strncmp(token, term, term_length);
+			char * lowerToken = stringLower(token);
+			int strcmp_lower_result = strcmp(lowerToken, lowerTerm);
+			int resultLower = strncmp(lowerToken, lowerTerm, term_length);
+			if((strcmp(token, term)) != 0) {
+				if(result1 == 0) {
+					return 0;
+				}
 			}
-		}
-		if (strcmp_lower_result != 0) {
-			if(resultLower == 0) {
-				return 0;
+			if (strcmp_lower_result != 0) {
+				if(resultLower == 0) {
+					return 0;
+				}
 			}
+			token = strtok(NULL, space);
 		}
-		token = strtok(NULL, space);
+		return 1;
+	} 
+	else {
+		return 2;
 	}
-	return 1;
+	
 }
 /*
 	 a verse satisfies this condition iff the verse has 
@@ -163,20 +182,27 @@ int checker_for_token_dash (char * term, char * string) {
 	char * replacement = strdup(term);
 	char * lowerTerm = stringLower(replacement);
 	int term_length = strlen(term);
-	
-	while (token != NULL) {
-		char * lowerToken = stringLower(token);
-
-		int strcmp_lower_result = strcmp(lowerToken, lowerTerm);
-		if((strcmp(token, term)) == 0) {
-			return 1;
+	if(term[0] == '-') {
+		while (token != NULL) {
+			term = remove_first_char(term);
+			term_length = strlen(term);
+			char * lowerToken = stringLower(token);
+			term_length = strlen(term);
+			int strcmp_lower_result = strcmp(lowerToken, lowerTerm);
+			if((strcmp(token, term)) == 0) {
+				return 1;
+			}
+			if(strcmp_lower_result == 0) {
+				return 1;
+			}
+			token = strtok(NULL, space);
 		}
-		if(strcmp_lower_result == 0) {
-			return 1;
-		}
-		token = strtok(NULL, space);
+		return 0;
+	} else {
+		return 2;
 	}
-	return 0;
+	
+
 }
 // removes the book and verse from the line
 char * remove_book_verse(char * str) {
@@ -198,27 +224,66 @@ char * remove_book_verse(char * str) {
     return new_str;
 }
 
+char* remove_last_char(const char* str) {
+    int len = strlen(str);
+    if (len == 0) {
+        return NULL;
+    }
+    char* new_str = (char*) malloc(len);
+    strncpy(new_str, str, len - 1);
+    new_str[len - 1] = '\0';
+    return new_str;
+}
 
+char* remove_first_char(const char* str) {
+	int len = strlen(str);
 
+	if(len == 0) {
+		return NULL;
+	}
+	char* new_str = (char*) malloc(len);
+	memmove(new_str, str +1, len);
+	new_str[len - 1] = '\0';
+	return new_str;
+}
 int main (int argc, char ** argv)
 {
+	// char input[MAX_INPUT_LENGTH];
+    // char *tokens[MAX_NUM_INPUT];
 
-	fp_niv = fopen("NIV.txt", "r") ;
+    // printf("Enter up to 8 tokens: ");
+    // fgets(input, 100, stdin);
 
-	char * jude = "Jude" ;
+    // int num_tokens = tokenizer(input, tokens);
+
+    // if(num_tokens < 0) {
+    //     return 1;
+    // }
+
+    // printf("Tokens: ");
+    // for(int j = 0; j < num_tokens; j++) {
+    //     printf("%s\n", tokens[j]);
+    // }
+    // printf("\n");
+
+
+	//fp_niv = fopen("NIV.txt", "r") ;
+
+	char * jude = "-Jude" ;
 	char * s = "Jude 2:1 After Jesus was born in Bethlehem in Jude, during the time of King Herod, Magi from the east came to Jerusalem";
-	//char * new_s = remove_book_verse(s);
+	char * new_s = remove_book_verse(s);
 	int x,y,z;
-	// x = checker_for_token_star(jude,s);
-	// printf("Checker for * %d\n", x);
+	//x = checker_for_token_star(jude,s);
+	//printf("Checker for * %d\n", x);
 	// y = checker_for_token(jude,s);
 	// printf("Checker for normal token %d\n", y);
 	z = checker_for_token_dash(jude,s);
 	printf("Checker for dash token(1 if it token is in line/ 0 if it is not):%d\n", z);
 	printf("%s\n", s);
 	//free(s);
-		
-	fclose(fp_niv) ;
+	//fclose(fp_niv) ;
+
+	
 	return 0;
 }
 	
